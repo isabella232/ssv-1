67b4d5d63602b2af320cb372c71ccf575c699609
Alon Muroch <alonmuroch@gmail.com>
2022-01-30 16:01:37 +0200

    qbft check value

--- docs/spec/qbft/messages.go
+++ docs/spec/qbft/messages.go
@@ -84,9 +84,3 @@ type RoundChangeData interface {
 	// GetRoundChangeJustification returns signed prepare messages for the last prepared state
 	GetRoundChangeJustification() []SignedMessage
 }
-
-// ValueCheck is an interface which validates the pre-prepare value passed to the node.
-// It's kept minimal to allow the implementation to have all the check logic.
-type ValueCheck interface {
-	Check(value []byte) error
-}
